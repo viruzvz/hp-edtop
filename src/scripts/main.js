d153ba8a4ca200b7004af6590fe64d6d4f573fe8
@cegn <cegn@senado.leg.br>
2023-02-09 00:53:19 -0300

    artigos finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import '@/scripts/bootstrap'
 import '@/scripts/search'
 import '@/scripts/stickyActiveTop'
 import '@/scripts/activeanchor'
+import '@/scripts/backtotop'
 
 // Smoth scroll page (necessida do css no html)
 const textElement = document.getElementById("text")
